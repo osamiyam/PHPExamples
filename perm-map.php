f1494324061fe061147d91050b882caea6f485de
Osami Yamamoto <39205109+osamiyam@users.noreply.github.com>
2023-11-20 13:17:27 +0900

    modify perm-map.php

--- perm-map.php
+++ perm-map.php
@@ -32,34 +32,30 @@ function append_all($lsts) {
 function array2s($lst) {
     $s = "[";
     for ($i = 0; $i < count($lst); $i++) {
-        $s = $s . (($i == 0)?"":", ") . $lst[$i];
+        $x = $lst[$i];
+	if (is_array($x)) $x = array2s($x);
+        $s = $s . (($i == 0)?"":", ") . $x;
     }
     $s .= "]";
     return $s;
 }
 
-$ans = map((function($x) {return $x * $x;}), [2, 3, 4]);
-puts(array2s($ans));
-$ans2 = filter((function($x) {return ($x % 2 == 0);}),[2, 3, 4]);
-puts(array2s($ans2));
-
-puts("-------------");
+puts(array2s(map((function($x) {return $x * $x;}), [2, 3, 4])));
+puts(array2s(filter((function($x) {return ($x % 2 == 0);}),[2, 3, 4])));
 
 function perm($lst) {
-     if (count($lst) == 0) return [[]]; 
-     else return
-     append_all(
-     map(function($x) use ($lst) {
-        return
-	 map(function ($y) use ($x) {return array_merge([$x], $y);},
-	  perm(filter(function ($i) use ($x) {return ($x != $i);}, $lst)));
-     }, $lst));
+   if (count($lst) == 0) return [[]]; 
+   else return
+   append_all(
+   map(function($x) use ($lst) {
+      return
+        map(function ($y) use ($x) {return array_merge([$x], $y);},
+	   perm(filter(function ($i) use ($x) {return ($x != $i);}, $lst)));
+   }, $lst));
 }
 
-$x = perm([1, 2, 3, 4]);
-foreach($x as $i) {
-    puts(array2s($i));
-}
+puts("-------------");
+puts(array2s(perm([1, 2, 3, 4])));
 
 
 
